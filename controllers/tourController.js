8c8bcfbaf9f7e0b5f9dab3524f8fee569de40fe9
aditya23-1994 <adityakhatwa@outlook.com>
2020-07-28 09:59:24 +0530

    implementing mongoose and creation of MVC architecture for furthur use.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,36 +1,12 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail 🤡',
-      message: 'invalid ID',
-    });
-  }
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    res.status(400).json({
-      status: 'failed',
-      message: 'missing price or name',
-    });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.getAllTour = (req, res) => {
   res.status(200).json({
     status: 'success 😅',
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
@@ -46,23 +22,21 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success 😆',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success 😆',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data set!',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
