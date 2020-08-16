25396daa1b89fa41f22bdaa5017edced017c9754
aditya23-1994 <adityakhatwa@outlook.com>
2020-08-16 12:26:59 +0530

    Implemented login user functionality with creation of new token after every new login.

--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -46,6 +47,14 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+//Instance method.Available to all the user documents....
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
