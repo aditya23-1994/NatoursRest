8996f06efd5ee644cce0e72daf8e5276813d3d1b
aditya23-1994 <adityakhatwa@outlook.com>
2020-08-16 22:05:45 +0530

    Added Protecting tour routes and created all the code against errors; Setup postman for automataing jwt tokens to auto fill.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,11 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const { exists } = require('../models/userModel');
 
-const signToken = id => {
+const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
@@ -47,4 +49,36 @@ exports.login = catchAsync(async (req, res, next) => {
     status: 'success',
     token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //1) Getting token and check of it exists
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('You are not logged in.Please log in', 401));
+  }
+  //2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  //3) Check if user still exists
+  // const freshUser = await User.findById(decoded.id);
+  // if (!freshUser) {
+  //   return next(new AppError("The user Belonging to the token doesn't exists"));
+  // }
+  // //4) Check if user changed password after JWT(Token) was issued
+  // if (freshUser.changedPasswordAfter(decoded.iat)) {
+  //   return next(
+  //     new AppError('User recently changed password! Please Login again.')
+  //   );
+  // }
+
+  // //Grant Access to protected Route
+  // req.user = freshUser;
+  next();
 });
